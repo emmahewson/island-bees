2d685057aaacc9a1f4433493e499e72c881e9b3e
emmahewson <emhewson@hotmail.com>
2023-07-31 12:08:07 +0000

    test: add home test_views

---
+++ home/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestHomeViews(TestCase):
+    """ Test home url views """
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_get_home_page_using_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
