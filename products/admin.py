92a0da7e5e0d18a033da5e424c3ce691c89fd893
emmahewson <emhewson@hotmail.com>
2023-08-03 14:04:28 +0000

    feat: added product & category models

---
+++ products/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Product, Category
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+        'is_featured',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
