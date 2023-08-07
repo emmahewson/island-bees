f7302e1b511f9c8b7ee041232d15e6b7cbeafb1f
emmahewson <emhewson@hotmail.com>
2023-08-07 16:27:21 +0000

    feat: add product_detail view & template

--- products/test_views.py
+++ products/test_views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class TestProductsViews(TestCase):
     """ Test home url views """
 
-    def test_get_products_page(self):
+    def test_products_page(self):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'products/products.html')
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -17,3 +17,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+        'rating': 3,  # Need to create code for this
+    }
+
+    return render(request, 'products/product_detail.html', context)
