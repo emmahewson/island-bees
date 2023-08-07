50cefd96d475008a665377db112701fac7edf247
emmahewson <emhewson@hotmail.com>
2023-08-07 12:13:37 +0000

    feat: change btt btn to fixed corner position

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
@@ -8,4 +9,11 @@ def index(request):
     A view to return the home page
     """
 
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+        'rating': 0,  # Need to create code for this
+    }
+
+    return render(request, 'home/index.html', context)
--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,20 @@
-// JavaScript functionality for scroll to top button
+// Scroll to top button
 
-$(document).ready(function(){
-    scrollToTop();
-  });
+let button = document.getElementById("btt-btn");
 
-// scroll to top button
-function scrollToTop() {
-    $('#btt-btn').click(function() {
-        window.scrollTo({top: 0, behaviour: 'smooth'});
-    });
+window.onscroll = function() {scrollFunction()};
+
+button.addEventListener('click', function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
+
+function scrollFunction() {
+    console.log(document.documentElement.scrollTop)
+    if (document.documentElement.scrollTop > 20) {
+        button.style.display = "block"
+    } else {
+        button.style.display = "none"
+    }
 }
+
