485cff7368376b790b11feb02b1c8749ff90437e
emmahewson <emhewson@hotmail.com>
2023-08-10 10:22:08 +0000

    feat: add page title appends

--- products/views.py
+++ products/views.py
@@ -14,8 +14,26 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     category = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == "name":
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower("name"))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == "desc":
+                    sortkey = f'-{sortkey}'
+
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             category = request.GET['category']
             products = products.filter(category__name=category)
@@ -33,10 +51,13 @@ def all_products(request):
                 description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_category': category,
+        'current_sorting': current_sorting
     }
 
     return render(request, 'products/products.html', context)
