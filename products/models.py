7ea2a32b838ee74f9dcef76cb482daa975b1f449
emmahewson <emhewson@hotmail.com>
2023-08-15 14:56:12 +0000

    feat: add responsive styling to add_review form

--- products/models.py
+++ products/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(
-        max_length=254)
+        max_length=50)
     description = models.TextField()
     price = models.DecimalField(
         max_digits=6, decimal_places=2)
