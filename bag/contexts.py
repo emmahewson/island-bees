2afaf30fac8670b331c1911fa3d5b1d428f9ef27
emmahewson <emhewson@hotmail.com>
2023-09-04 15:52:37 +0000

    feat: add shopping bag styling and content

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,36 +10,25 @@ def bag_contents(request):
     total = 0
     total_delivery_chargable = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        product_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
+        if product.delivery_charge is True:
+            total_delivery_chargable += (product.price * item_data)
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-
-        bag = request.session.get('bag', {})
-
-        for item_id, item_data in bag.items():
-            if isinstance(item_data, int):
-                product = get_object_or_404(Product, pk=item_id)
-                total += item_data * product.price
-                total_delivery_chargable += product.price
-                product_count += item_data
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': item_data,
-                    'product': product,
-                })
-            # else:
-            #     product = get_object_or_404(Product, pk=item_id)
-            #     for quantity in item_data['items_by_size'].items():
-            #         total += quantity * product.price
-            #         product_count += quantity
-            #         bag_items.append({
-            #             'item_id': item_id,
-            #             'quantity': quantity,
-            #             'product': product,
-            #         })
-
         delivery = total_delivery_chargable * Decimal(
             settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+
     else:
         delivery = 0
         free_delivery_delta = 0
