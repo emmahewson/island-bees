d4754d80b9174c6c8a749ede8831fc8ab036a53c
emmahewson <emhewson@hotmail.com>
2023-09-16 10:44:37 +0000

    feat: added profile to webhook handler order creation

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -57,6 +58,27 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        # Allows anonymous users to still checkout
+        profile = None
+        username = intent.metadata.username
+
+        # Checks if user is logged in
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            # Checks if user wants to save info
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         # Check if order exists and if not create it
         # Avoids errors if user closes browser
         # or process is interrupted
@@ -104,6 +126,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
