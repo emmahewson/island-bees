59a89d12845617cc657a26dc00d4ef4d5f8bcedb
emmahewson <emhewson@hotmail.com>
2023-09-17 13:37:15 +0000

    feat: add edit_product view, url and template

--- products/views.py
+++ products/views.py
@@ -121,3 +121,37 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """ Edit a product """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can edit products.')
+        return redirect(reverse('home'))
+
+    # Get product from DB
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Handle Form Submission
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            request.session['show_bag_summary'] = False
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Failed to update product. Please check the form.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
