4e4368ce1fa08d9555544789a8f8e6afc72c34f1
emmahewson <emhewson@hotmail.com>
2023-09-18 08:58:23 +0000

    build: deployment settings changed

--- island_bees/settings.py
+++ island_bees/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +33,8 @@ ALLOWED_HOSTS = [
     '8000-emmahewson-islandbees-bj2tahhh1s7.ws-eu102.gitpod.io',
     '8000-emmahewson-islandbees-bj2tahhh1s7.ws-eu103.gitpod.io',
     '8000-emmahewson-islandbees-bj2tahhh1s7.ws-eu104.gitpod.io',
+    'island-bees.herokuapp.com',
+    'localhost',
 ]
 
 
@@ -119,13 +122,17 @@ WSGI_APPLICATION = 'island_bees.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
