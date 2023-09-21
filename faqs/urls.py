63204ac67821de1ca78ebcbc69f1d39b39a50e09
emmahewson <emhewson@hotmail.com>
2023-09-21 15:35:44 +0000

    feat: add edit & delete functionality for FAQs

--- faqs/urls.py
+++ faqs/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.faqs, name='faqs'),
     path('add/', views.add_faq, name='add_faq'),
+    path('edit/<int:faq_id>/', views.edit_faq, name='edit_faq'),
+    path('delete/<int:faq_id>/', views.delete_faq, name='delete_faq'),
 ]
--- faqs/views.py
+++ faqs/views.py
@@ -11,7 +11,7 @@ def faqs(request):
     A view to return the FAQs
     """
 
-    # Gets faqs from DB for featured products section
+    # Gets faqs from DB for featured faqs section
     faqs = Faq.objects.all()
     context = {
         'faqs': faqs,
@@ -54,9 +54,76 @@ def add_faq(request):
     # Sets page template
     template = 'faqs/add_faq.html'
 
-    # Sets current product & form content
+    # Sets current faq & form content
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_faq(request, faq_id):
+    """ Edit a FAQ """
+
+    # Checks user is superuser
+    # redirects to FAQS if not
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'Sorry, only store owners can edit FAQS.')
+        return redirect(reverse('faqs'))
+
+    # Get faq from DB
+    faq = get_object_or_404(Faq, pk=faq_id)
+
+    # Handle Form Submission
+    if request.method == 'POST':
+        form = FaqForm(request.POST, request.FILES, instance=faq)
+
+        if form.is_valid():
+            form.save()
+
+            request.session['show_bag_summary'] = False
+            messages.success(request, 'Successfully updated FAQ!')
+
+            # Redirects to 'faqs'
+            return redirect(reverse('faqs'))
+
+        else:
+            messages.error(
+                request, 'Failed to update FAQ. Please check the form.')
+
+    # Handles View Rendering
+    else:
+        form = FaqForm(instance=faq)
+        messages.info(request, f'You are editing {faq.question}')
+
+    # Sets page template
+    template = 'faqs/edit_faq.html'
+
+    # Sets current faq & form content
+    context = {
+        'form': form,
+        'faq': faq,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_faq(request, faq_id):
+    """ Delete a FAQ """
+
+    # Checks user is superuser
+    # redirects to FAQS if not
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'Sorry, only store owners can delete FAQS.')
+        return redirect(reverse('faqs'))
+
+    faq = get_object_or_404(Faq, pk=faq_id)
+    faq.delete()
+    message = 'FAQ deleted!'
+
+    messages.success(request, message)
+    return redirect(reverse('faqs'))
