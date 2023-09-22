5b71a292a37b360e94c08071193aab1bb0e8af97
emmahewson <emhewson@hotmail.com>
2023-09-22 12:54:16 +0000

    feat: add manage.html content & styling

--- manage/views.py
+++ manage/views.py
@@ -6,15 +6,23 @@ from .models import Message
 # from .forms import MessageForm
 
 
+@login_required
 def manage(request):
     """
     A view to return the Site Management Page
     """
 
+    # Checks user is superuser
+    # redirects to home if not
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'Sorry, only store owners can access that page.')
+        return redirect(reverse('home'))
+
     # Gets messages from DB
-    messages = Message.objects.all()
+    customer_messages = Message.objects.all()
     context = {
-        'messages': messages,
+        'customer_messages': customer_messages,
     }
 
     return render(request, 'manage/manage.html', context)
