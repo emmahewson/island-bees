b51afec929d25393bda4c2cf624a6191ad16879d
emmahewson <emhewson@hotmail.com>
2023-09-23 09:56:45 +0000

    feat: add filters to manage messages

--- manage/views.py
+++ manage/views.py
@@ -20,9 +20,14 @@ def manage(request):
         return redirect(reverse('home'))
 
     # Gets messages from DB
-    customer_messages = Message.objects.all().order_by(
-        '-is_open', 'created_on').values()
-        
+    customer_messages = Message.objects.all().order_by('created_on').values()
+
+    # Handles product filtering by is_open
+    if 'open' in request.GET:
+        open = request.GET['open']
+        customer_messages = customer_messages.filter(
+            is_open=open).order_by('created_on').values()
+
     context = {
         'customer_messages': customer_messages,
     }
@@ -34,16 +39,25 @@ def toggle_message(request, message_id):
     """
     A View to toggle the message open / closed
     """
+
+    # Checks user is superuser
+    # redirects to home if not
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'Sorry, only store owners can access that page.')
+        return redirect(reverse('home'))
+
+    # Gets the message from DB
     message = get_object_or_404(Message, id=message_id)
 
     # Gets value of hidden input
     message_open = request.POST.get('is_open')
 
+    # Sets value of is_open field on DB
     if message_open == "True":
         message.is_open = True
     else:
         message.is_open = False
-
     message.save()
 
     return redirect('manage')
